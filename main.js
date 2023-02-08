0a7350a686966d1ccfb16d7b38b22d060a664d72
TerezaKut <114390034+TerezaKut@users.noreply.github.com>
2023-02-08 09:37:50 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 // tady je místo pro náš program
 
+let jmeno = prompt('Zadejte svoje jméno:');
+let prijmeni = prompt('Zadejte svoje příjmení:');
+let vek = Number(prompt('Zadejte svůj věk:'));
+let barva = prompt('Zadejte oblíbenou barvu (anglicky):');
 
+let vystup = document.querySelector('.vystupProgramu');
+
+vystup.innerHTML = '<p>'+ jmeno + ' ' + prijmeni + ', věk: ' + vek + '</p>'
+
+vystup.style.color = barva;
 
 
